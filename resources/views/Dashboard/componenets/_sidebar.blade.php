5c92414450a5682d85ecc30f30860ae8947e3cb4
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-10-18 17:24:31 +0300

    edit in aside

--- resources/views/Dashboard/componenets/_sidebar.blade.php
+++ resources/views/Dashboard/componenets/_sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="sidebar">
+<aside class="sidebar signed">
   <ul class="nav flex-column">
     <li class="nav-item">
       <a class="nav-link" href="{{ url()->route('dashboard.users') }}">Users</a>
@@ -8,3 +8,14 @@
     </li>
   </ul>
 </aside>
+
+
+<script>
+  if (localStorage.getItem('token')) {
+    document.querySelectorAll('.unsigned').forEach(e => e.style.display = "none");
+    document.querySelectorAll('.signed').forEach(e => e.style.display = "inline-block");
+  } else {
+    document.querySelectorAll('.unsigned').forEach(e => e.style.display = "inline-block");
+    document.querySelectorAll('.signed').forEach(e => e.style.display = "none");
+  }
+</script>
